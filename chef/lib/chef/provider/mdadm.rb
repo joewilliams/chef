dad6f779581d96aac0a3c4ec03603d497d9c79c8
zeusfaber <joe@joetify.com>
2009-09-28 12:30:55 -0700

    mdadm create support

---
+++ chef/lib/chef/provider/mdadm.rb
@@ -0,0 +1,64 @@
+#
+# Author:: Joe Williams (<joe@joetify.com>)
+# Copyright:: Copyright (c) 2009 Joe Williams
+# License:: Apache License, Version 2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+require 'chef/log'
+require 'chef/mixin/command'
+require 'chef/provider'
+
+class Chef
+  class Provider
+    class Mdadm < Chef::Provider
+
+      include Chef::Mixin::Command
+
+      def initialize(node, new_resource)
+        super(node, new_resource)
+      end
+
+      def load_current_resource
+        true
+      end
+
+      def device_check(device)
+        command = "mdadm --detail --scan"
+        check = false
+        status = popen4(command) do |pid, stdin, stdout, stderr|
+          stdout.each do |line|
+            if line.include? device
+              check = true
+            end
+          end
+        end
+        check
+      end
+
+      def action_create
+        unless device_check(@new_resource.raid_device)
+          command = "yes | mdadm --create #{@new_resource.raid_device} --chunk=#{@new_resource.chunk} --level #{@new_resource.level} --raid-devices #{@new_resource.devices.length} #{@new_resource.devices.collect{|x| x + " "}.to_s.chop}"
+          Chef::Log.debug("mdadm command: #{command}")
+          status = popen4(command) do |pid, stdin, stdout, stderr|
+          end
+          Chef::Log.info("Created mdadm raid device (#{@new_resource.raid_device})")
+        else
+          Chef::Log.debug("mdadm raid device already exists. (#{@new_resource.raid_device})")
+        end
+      end
+
+    end
+  end
+end
